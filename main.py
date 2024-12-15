427b24cd0a6505f9ee678899f8ceeb9c9f6fca63
pizzzo <chevezpeter@gmail.com>
2024-12-15 15:28:40 -0800

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    logger()
 
 
 def get_num_words(text):
@@ -26,5 +22,18 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def logger():
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in document\n\n")
+    for i in chars_dict:
+        if(i.isalpha()):
+            print(f"The '{i}' character was found {chars_dict[i]} times\n")
+
+book_path = "books/frankenstein.txt"
+text = get_book_text(book_path)
+num_words = get_num_words(text)
+chars_dict = get_chars_dict(text)
+
 
 main()
+
